503611c0ace724a64fe5f47e81217ab3f536bdd3
MaxOvs <rock_maksimus@mail.ru>
2022-09-11 22:06:15 +0300

    [main] Create new project in JS

---
+++ src/model.js
@@ -0,0 +1,10 @@
+export const model = [
+    {type: 'title', value: 'hello World JS' },
+    {type: 'text', value: 'textMook'},
+    {type: 'columns', value: [
+        '1',
+        '2',
+        '3'
+    ]},
+    {type: 'image', value: './assets/image.jpg'}
+];
